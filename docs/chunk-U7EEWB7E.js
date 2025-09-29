811fdf9e301415a4199bb17c7efe64b5ec314646
Karim Fathy <139636760+So3S03@users.noreply.github.com>
2025-09-29 23:38:10 +0300

    created build version for production

---
+++ docs/chunk-U7EEWB7E.js
@@ -0,0 +1,1 @@
+import{Fb as r,Y as n,gb as o,hb as i,xb as a}from"./chunk-UH2Y4DRO.js";var m=(()=>{class t{static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275cmp=n({type:t,selectors:[["app-not-found"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,d){e&1&&(o(0,"p"),a(1,"not-found works!"),i())}})}}return t})();export{m as NotFoundComponent};
